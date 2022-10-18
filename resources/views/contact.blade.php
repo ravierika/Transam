1b84f0e41cbfcc4f694f772a486996929c6ad991
Jahid <jahidhasan.developer@gmail.com>
2022-10-18 20:42:01 +0700

    update

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Travel Freeby</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&amp;display=swap"
+      rel="stylesheet"
+    />
+    <script src="https://kit.fontawesome.com/4b5d72e539.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/4b5d72e539.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="assetsh/css/welcome.css" />
+  </head>
+  <body>
+    <header>
+      <nav class="fixed_menu">
+        <div class="container">
+          <div
+            class="nav-wrapper d-flex align-items-center justify-content-between"
+          >
+            <a href="index.html" class="brand-logo">
+              <img src="./assets/images/logo.webp" alt="" />
+            </a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+              <li><a href="index.html">Home</a></li>
+              <li><a href="services.html">Services</a></li>
+              <li><a class="active" href="contact.html">Contact</a></li>
+            </ul>
+            <i onclick="menuToggle('nav-mobile')" class="fa fa-bars"></i>
+          </div>
+        </div>
+      </nav>
+    </header>
+
+    <!-- travel section -->
+    <section class="content mb-5">
+        <div class="head_title">
+            <h2>FREQUENTLY ASKED QUESTIONS</h2>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 mb-3">
+                    <div class="card_view text-center">
+                        <p class="title">Why should we use a travel company?</p>
+                        <p class="text">Even in the age of online booking, travel companies are a great source of information and discounts. We help you Create spend limits and restrict class of travel for greater budget control, Access corporate deals on flights, hotels and car hire (international and domestic).</p>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card_view border_both  text-center">
+                        <p class="title">Will I pay more if I book through a travel company?</p>
+                        <p class="text">Our fees are transparent. In many cases, you save more than you spend. We can work within your budget, and we always focus on making sure that you get a solid value in return for our fees. </p>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card_view text-center">
+                        <p class="title">Do you book group travel?</p>
+                        <p class="text">Yes! Do you have an upcoming conference for your business or a student trip? We can handle the details and ensure that you have access to group seating, entrances, and discounts.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="contact py-5">
+        <div class="container text-center">
+            <h2 class="text-uppercase mb-5">CONTACT Us</h2>
+            <h3>We'd love to hear from you!</h3>
+            <p> Unit - 224, JMD Megapolis, Sohna Road, Sec - 48 Gurgaon  Haryana, India 122001</p>
+            <p> Phone:- (0124) 4384563 Mobile - +91-9999360524  Email - <a href="mailto:info@travelfreeby.com">info@travelfreeby.com</a>  </p>
+            <p class="whatsapp mt-4">
+                <a style="display:inline-block;" href="https://wa.me/919999360524"
+                  ><span
+                    data-ux="Element"
+                    class="x-el x-el-span c1-d0 c1-d1 c1-4p c1-d2 c1-63 c1-d3 c1-d4 c1-d5 c1-d6 c1-ci c1-c"
+                    ><svg
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      width="24px"
+                      height="24px"
+                      data-ux="Icon"
+                      class="x-el x-el-svg c1-34 c1-1e c1-2c c1-ci c1-c"
+                    >
+                      <svg
+                      viewBox="0 0 496 497">
+                        <defs>
+                          <linearGradient
+                            id="a"
+                            x1="247.32"
+                            x2="247.32"
+                            y1="446.09"
+                            y2="39.9"
+                            gradientUnits="userSpaceOnUse"
+                          >
+                            <stop offset="0" stop-color="#20b038"></stop>
+                            <stop offset="1" stop-color="#60d66a"></stop>
+                          </linearGradient>
+                          <linearGradient
+                            id="b"
+                            x1="247.32"
+                            x2="247.32"
+                            y1="453.37"
+                            y2="32.61"
+                            gradientUnits="userSpaceOnUse"
+                          >
+                            <stop offset="0" stop-color="#f9f9f9"></stop>
+                            <stop offset="1" stop-color="#fff"></stop>
+                          </linearGradient>
+                        </defs>
+                        <path
+                          d="M37.88 453.37l29.59-108A208 208 0 0 1 39.63 241.1c0-115 93.6-208.49 208.56-208.49a208.57 208.57 0 0 1 208.57 208.66c-.05 115-93.62 208.49-208.57 208.49h-.08a208.41 208.41 0 0 1-99.67-25.38zm115.68-66.73l6.34 3.75a173.18 173.18 0 0 0 88.23 24.16h.06c95.55 0 173.31-77.75 173.35-173.3A173.34 173.34 0 0 0 248.26 67.83c-95.62 0-173.38 77.73-173.42 173.28a172.94 172.94 0 0 0 26.5 92.23l4.13 6.55L88 403.84z"
+                        ></path>
+                        <path
+                          fill="url(#a)"
+                          d="M45.13 446.09l28.57-104.3a200.82 200.82 0 0 1-26.88-100.62c0-111 90.36-201.27 201.34-201.27A201.35 201.35 0 0 1 449.5 241.32c0 111-90.37 201.28-201.33 201.28h-.09a201.31 201.31 0 0 1-96.21-24.49z"
+                        ></path>
+                        <path
+                          fill="url(#b)"
+                          d="M37.88 453.37l29.59-108A208 208 0 0 1 39.63 241.1c0-115 93.6-208.49 208.56-208.49a208.57 208.57 0 0 1 208.57 208.66c-.05 115-93.62 208.49-208.57 208.49h-.08a208.41 208.41 0 0 1-99.67-25.38zm115.68-66.73l6.34 3.75a173.18 173.18 0 0 0 88.23 24.16h.06c95.55 0 173.31-77.75 173.35-173.3A173.34 173.34 0 0 0 248.26 67.83c-95.62 0-173.38 77.73-173.42 173.28a172.94 172.94 0 0 0 26.5 92.23l4.13 6.55L88 403.84z"
+                        ></path>
+                        <path
+                          fill="#fff"
+                          d="M196.07 153.94c-3.91-8.68-8-8.85-11.73-9-3-.14-6.51-.13-10-.13a19.15 19.15 0 0 0-13.89 6.52c-4.78 5.22-18.24 17.82-18.24 43.46s18.67 50.42 21.28 53.9 36.05 57.77 89 78.66c44 17.36 53 13.91 62.53 13s30.83-12.61 35.18-24.78 4.34-22.59 3-24.77-4.78-3.48-10-6.08-30.83-15.22-35.61-16.95-8.25-2.61-11.73 2.61-13.45 16.94-16.5 20.42-6.08 3.92-11.29 1.31-22-8.11-41.9-25.86c-15.5-13.82-26-30.87-29-36.09s-.32-8 2.29-10.63c2.34-2.34 5.21-6.09 7.82-9.13s3.47-5.21 5.21-8.69.87-6.52-.44-9.13-11.35-28.34-15.98-38.64z"></path>
+                      </svg></svg></span>
+                  Message us on WhatsApp</a>
+              </p>
+              <p>JMD Megapolis, Sohna Road, Sector 48, Gurugram, Haryana, India</p>
+        </div>
+    </section>
+
+    <section class="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14001.011120287369!2d76.95720127897992!3d28.682084118370252!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d0f292e529ee1%3A0x4e037bd923885447!2sTikri%20Kalan%2C%20Bahadurgarh%2C%20Delhi%2C%20India!5e0!3m2!1sen!2sbd!4v1650977905683!5m2!1sen!2sbd" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </section>
+
+    <footer class="footer_section py-4">
+      <div class="container">
+        <div class="copyright text-center">
+          <p>Copyright © 2022 Travel Freeby - All Rights Reserved.</p>
+          <ul>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Cookie Policy</a></li>
+            <li><a href="#">Terms of use</a></li>
+            <li><a href="#">User Agreement</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+     <!-- external section -->
+     <div id="chat"  class="fixed_message">
+      <div class="chat_container">
+        <div class="chat_header">
+          <h2>Travel Freeby</h2>
+        </div>
+        <div id="chat_input">
+          <form action="" method="get">
+            <input type="text" name="" id="" placeholder="Name">
+            <input type="email" name="" id="" placeholder="Email">
+            <textarea name="" id="" placeholder="How can we help?*"></textarea>
+            <div class="text-center">
+              <button type="submit">SEND</button>
+            </div>
+          </form>
+          <p class="text-center">This site is protected by reCAPTCHA and the Google <a href="#">Privacy Policy</a> and <a href="#">Terms of Service</a> apply.</p>
+        </div>
+      </div>
+      <div class="chat_icon">
+        <i id="chatIcon" onclick="chatToggle('chat','chatIcon')" class="fa fa-comment"></i>
+      </div>
+    </div>
+
+    <script src="assetsh/js/welcome.js"></script>
+  </body>
+</html>
---
+++ resources/views/services.blade.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Travel Freeby</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&amp;display=swap"
+      rel="stylesheet"
+    />
+    <script src="https://kit.fontawesome.com/4b5d72e539.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="assetsh/css/welcome.css" />
+  </head>
+  <body>
+    <header>
+      <nav class="fixed_menu">
+        <div class="container">
+          <div
+            class="nav-wrapper d-flex align-items-center justify-content-between"
+          >
+            <a href="index.html" class="brand-logo">
+              <img src="./assets/images/logo.webp" alt="" />
+            </a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+              <li><a href="index.html">Home</a></li>
+              <li><a class="active" href="services.html">Services</a></li>
+              <li><a href="contact.html">Contact</a></li>
+            </ul>
+            <i onclick="menuToggle('nav-mobile')" class="fa fa-bars"></i>
+          </div>
+        </div>
+      </nav>
+    </header>
+
+    <!-- travel section -->
+    <section class="content mb-5">
+        <div class="head_title">
+            <h2>Corporate Travel</h2>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 mb-3">
+                    <div class="card_view text-center">
+                        <p class="title">Reduce Travel Costs</p>
+                        <p class="text">We offer special discounted promo and corporate fares in flights. Special B2B pre-purchased hotel deals help corporates reduce travel costs.</p>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card_view border_both text-center">
+                        <p class="title">Store traveler profiles</p>
+                        <p class="text">Our technology driven portal help you save traveler profiles along with loyalty memberships and traveler preferences helps increased efficiency.</p>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card_view text-center">
+                        <p class="title">Unparalleled Support</p>
+                        <p class="text">We've got unparalleled customer support at your service. Our customer service team's response time helps you travel easy and smooth.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="mb-5">
+        <div class="container">
+            <div class="head_title">
+                <h2>Personalized Travel</h2>
+            </div>
+            <div class="row">
+                <div class="col-md-4  mb-3">
+                    <div class="card_view text-center">
+                        <p class="title">Unique life experiences</p>
+                        <p class="text">We specialize in offering once in a lifetime experiences that will create lasting memories.</p>
+                    </div>
+                </div>
+                <div class="col-md-4  mb-3">
+                    <div class="card_view border_both text-center">
+                        <p class="title">Personalized trips</p>
+                        <p class="text">Travel is different for everyone. We want to make sure that you are getting the most from your trip by crafting the dream vacation that is perfect for you.</p>
+                    </div>
+                </div>
+                <div class="col-md-4  mb-3">
+                    <div class="card_view text-center">
+                        <p class="title">Incredible detail and service</p>
+                        <p class="text">We've got your back. In the event something does come up. Our team is here to help make sure your trip is not interrupted.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- reviews -->
+
+    <section class="reviews py-5">
+      <div class="container">
+        <div class="head_title">
+          <h2>Reviews</h2>
+        </div>
+
+        <div class="ratting">
+            <div class="d-flex align-items-center justify-content-center">
+              <div class="number_ratting mx-2">
+                <p class="d-flex align-items-center"><span><svg viewBox="0 0 24 24" fill="currentColor" width="48" height="48" data-ux="Icon" class="x-el x-el-svg c2-1 c2-2 c2-11 c2-r c2-s c2-3 c2-4 c2-5 c2-6 c2-7 c2-8"><g fill="none" fill-rule="evenodd" transform="translate(2 2)"><circle cx="10" cy="10" r="9.5" fill="#F3F3F3" stroke="#CCC"></circle><g transform="translate(5.556 5.5)"><path fill="#4285F4" d="M8.494 4.375c0-.303-.028-.595-.079-.875H4.334v1.655h2.332c-.1.535-.406.988-.865 1.291V7.52h1.4c.82-.744 1.293-1.841 1.293-3.144z"></path><path fill="#34A853" d="M4.334 8.556c1.17 0 2.15-.383 2.868-1.037l-1.4-1.073c-.389.257-.885.408-1.468.408-1.129 0-2.084-.752-2.425-1.763H.46v1.108a4.338 4.338 0 0 0 3.873 2.357z"></path><path fill="#FBBC05" d="M1.909 5.09a2.542 2.542 0 0 1-.136-.812c0-.282.05-.556.136-.813V2.357H.46a4.231 4.231 0 0 0 0 3.842l1.448-1.108z"></path><path fill="#EA4335" d="M4.334 1.701c.636 0 1.207.216 1.656.64l1.243-1.227C6.483.424 5.502 0 4.333 0 2.64 0 1.175.959.462 2.357l1.448 1.108c.34-1.011 1.296-1.764 2.425-1.764z"></path></g></g></svg></span><span class="rate">4.3</span></p>
+              </div>
+              <div class="ratting_title mx-2">
+                <h3>travel freeby</h3>
+                <div class="d-flex">
+                  <svg data-ux="SVG" viewBox="0 0 50 50" class="x-el x-el-svg c2-1 c2-2 c2-k c2-27 c2-3 c2-4 c2-5 c2-6 c2-7 c2-8"><defs><linearGradient id="fillStyle-rating1-star-2"><stop offset="0%" stop-color="#FBCF1E"></stop><stop offset="100%" stop-color="#FBCF1E"></stop></linearGradient></defs><path d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z" fill="url(#fillStyle-rating1-star-2)"></path></svg>
+
+                <svg data-ux="SVG" viewBox="0 0 50 50" class="x-el x-el-svg c2-1 c2-2 c2-k c2-27 c2-3 c2-4 c2-5 c2-6 c2-7 c2-8"><defs><linearGradient id="fillStyle-rating1-star-2"><stop offset="0%" stop-color="#FBCF1E"></stop><stop offset="100%" stop-color="#FBCF1E"></stop></linearGradient></defs><path d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z" fill="url(#fillStyle-rating1-star-2)"></path></svg>
+                <svg data-ux="SVG" viewBox="0 0 50 50" class="x-el x-el-svg c2-1 c2-2 c2-k c2-27 c2-3 c2-4 c2-5 c2-6 c2-7 c2-8"><defs><linearGradient id="fillStyle-rating1-star-2"><stop offset="0%" stop-color="#FBCF1E"></stop><stop offset="100%" stop-color="#FBCF1E"></stop></linearGradient></defs><path d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z" fill="url(#fillStyle-rating1-star-2)"></path></svg>
+                <svg data-ux="SVG" viewBox="0 0 50 50" class="x-el x-el-svg c2-1 c2-2 c2-k c2-27 c2-3 c2-4 c2-5 c2-6 c2-7 c2-8"><defs><linearGradient id="fillStyle-rating1-star-2"><stop offset="0%" stop-color="#FBCF1E"></stop><stop offset="100%" stop-color="#FBCF1E"></stop></linearGradient></defs><path d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z" fill="url(#fillStyle-rating1-star-2)"></path></svg>
+                <svg data-ux="SVG" viewBox="0 0 50 50" class="x-el x-el-svg c2-1 c2-2 c2-k c2-27 c2-3 c2-4 c2-5 c2-6 c2-7 c2-8"><defs><linearGradient id="fillStyle-rating1-star-4"><stop offset="0%" stop-color="#FBCF1E"></stop><stop offset="29.999999999999982%" stop-color="#FBCF1E"></stop><stop offset="29.999999999999982%" stop-color="rgb(224,224,224)"></stop><stop offset="100%" stop-color="rgb(224,224,224)"></stop></linearGradient></defs><path d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z" fill="url(#fillStyle-rating1-star-4)"></path></svg>
+                </div>
+                <p>3 reviews</p>
+              </div>
+            </div>
+        </div>
+        <div class="review_card">
+          <div class="row align-items-center justify-content-center">
+            <div class="col-md-3 mb-4">
+              <div class="card_view text-center">
+                <img src="./assets/images/user1.png" alt="">
+                <div class="d-flex justify-content-center mb-3">
+                  <svg data-ux="SVG" viewBox="0 0 50 50" class="x-el x-el-svg c2-1 c2-2 c2-k c2-27 c2-3 c2-4 c2-5 c2-6 c2-7 c2-8"><defs><linearGradient id="fillStyle-rating1-star-2"><stop offset="0%" stop-color="#FBCF1E"></stop><stop offset="100%" stop-color="#FBCF1E"></stop></linearGradient></defs><path d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z" fill="url(#fillStyle-rating1-star-2)"></path></svg><svg data-ux="SVG" viewBox="0 0 50 50" class="x-el x-el-svg c2-1 c2-2 c2-k c2-27 c2-3 c2-4 c2-5 c2-6 c2-7 c2-8"><defs><linearGradient id="fillStyle-rating1-star-2"><stop offset="0%" stop-color="#FBCF1E"></stop><stop offset="100%" stop-color="#FBCF1E"></stop></linearGradient></defs><path d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z" fill="url(#fillStyle-rating1-star-2)"></path></svg><svg data-ux="SVG" viewBox="0 0 50 50" class="x-el x-el-svg c2-1 c2-2 c2-k c2-27 c2-3 c2-4 c2-5 c2-6 c2-7 c2-8"><defs><linearGradient id="fillStyle-rating1-star-2"><stop offset="0%" stop-color="#FBCF1E"></stop><stop offset="100%" stop-color="#FBCF1E"></stop></linearGradient></defs><path d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z" fill="url(#fillStyle-rating1-star-2)"></path></svg><svg data-ux="SVG" viewBox="0 0 50 50" class="x-el x-el-svg c2-1 c2-2 c2-k c2-27 c2-3 c2-4 c2-5 c2-6 c2-7 c2-8"><defs><linearGradient id="fillStyle-rating1-star-2"><stop offset="0%" stop-color="#FBCF1E"></stop><stop offset="100%" stop-color="#FBCF1E"></stop></linearGradient></defs><path d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z" fill="url(#fillStyle-rating1-star-2)"></path></svg><svg data-ux="SVG" viewBox="0 0 50 50" class="x-el x-el-svg c2-1 c2-2 c2-k c2-27 c2-3 c2-4 c2-5 c2-6 c2-7 c2-8"><defs><linearGradient id="fillStyle-rating1-star-2"><stop offset="0%" stop-color="#FBCF1E"></stop><stop offset="100%" stop-color="#FBCF1E"></stop></linearGradient></defs><path d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z" fill="url(#fillStyle-rating1-star-2)"></path></svg>
+                </div>
+                <p class="review_text">
+                  "Great discounts and
+unbelievable prices :)"
+                </p>
+                <div class="date">
+                  <div class="d-flex justify-content-center align-items-center">
+                    <svg viewBox="0 0 24 24" fill="currentColor" width="28" height="28" data-ux="Icon" class="x-el x-el-svg c2-1 c2-2 c2-1d c2-r c2-s c2-3 c2-4 c2-5 c2-6 c2-7 c2-8"><g fill="none" fill-rule="evenodd" transform="translate(2 2)"><circle cx="10" cy="10" r="9.5" fill="#F3F3F3" stroke="#CCC"></circle><g transform="translate(5.556 5.5)"><path fill="#4285F4" d="M8.494 4.375c0-.303-.028-.595-.079-.875H4.334v1.655h2.332c-.1.535-.406.988-.865 1.291V7.52h1.4c.82-.744 1.293-1.841 1.293-3.144z"></path><path fill="#34A853" d="M4.334 8.556c1.17 0 2.15-.383 2.868-1.037l-1.4-1.073c-.389.257-.885.408-1.468.408-1.129 0-2.084-.752-2.425-1.763H.46v1.108a4.338 4.338 0 0 0 3.873 2.357z"></path><path fill="#FBBC05" d="M1.909 5.09a2.542 2.542 0 0 1-.136-.812c0-.282.05-.556.136-.813V2.357H.46a4.231 4.231 0 0 0 0 3.842l1.448-1.108z"></path><path fill="#EA4335" d="M4.334 1.701c.636 0 1.207.216 1.656.64l1.243-1.227C6.483.424 5.502 0 4.333 0 2.64 0 1.175.959.462 2.357l1.448 1.108c.34-1.011 1.296-1.764 2.425-1.764z"></path></g></g></svg>
+                    <p class="ms-3 mb-0"><strong>Vineet S -</strong> <span> 29/6/2016</span></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 mb-4">
+              <div class="card_view  text-center">
+                <img src="./assets/images/user2.png" alt="">
+                <div class="d-flex justify-content-center mb-3">
+                  <svg data-ux="SVG" viewBox="0 0 50 50" class="x-el x-el-svg c2-1 c2-2 c2-k c2-27 c2-3 c2-4 c2-5 c2-6 c2-7 c2-8"><defs><linearGradient id="fillStyle-rating1-star-2"><stop offset="0%" stop-color="#FBCF1E"></stop><stop offset="100%" stop-color="#FBCF1E"></stop></linearGradient></defs><path d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z" fill="url(#fillStyle-rating1-star-2)"></path></svg><svg data-ux="SVG" viewBox="0 0 50 50" class="x-el x-el-svg c2-1 c2-2 c2-k c2-27 c2-3 c2-4 c2-5 c2-6 c2-7 c2-8"><defs><linearGradient id="fillStyle-rating1-star-2"><stop offset="0%" stop-color="#FBCF1E"></stop><stop offset="100%" stop-color="#FBCF1E"></stop></linearGradient></defs><path d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z" fill="url(#fillStyle-rating1-star-2)"></path></svg><svg data-ux="SVG" viewBox="0 0 50 50" class="x-el x-el-svg c2-1 c2-2 c2-k c2-27 c2-3 c2-4 c2-5 c2-6 c2-7 c2-8"><defs><linearGradient id="fillStyle-rating1-star-2"><stop offset="0%" stop-color="#FBCF1E"></stop><stop offset="100%" stop-color="#FBCF1E"></stop></linearGradient></defs><path d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z" fill="url(#fillStyle-rating1-star-2)"></path></svg><svg data-ux="SVG" viewBox="0 0 50 50" class="x-el x-el-svg c2-1 c2-2 c2-k c2-27 c2-3 c2-4 c2-5 c2-6 c2-7 c2-8"><defs><linearGradient id="fillStyle-rating3-star-3"><stop offset="0%" stop-color="#FBCF1E"></stop><stop offset="0%" stop-color="#FBCF1E"></stop><stop offset="0%" stop-color="rgb(224,224,224)"></stop><stop offset="100%" stop-color="rgb(224,224,224)"></stop></linearGradient></defs><path d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z" fill="url(#fillStyle-rating3-star-3)"></path></svg><svg data-ux="SVG" viewBox="0 0 50 50" class="x-el x-el-svg c2-1 c2-2 c2-k c2-27 c2-3 c2-4 c2-5 c2-6 c2-7 c2-8"><defs><linearGradient id="fillStyle-rating3-star-3"><stop offset="0%" stop-color="#FBCF1E"></stop><stop offset="0%" stop-color="#FBCF1E"></stop><stop offset="0%" stop-color="rgb(224,224,224)"></stop><stop offset="100%" stop-color="rgb(224,224,224)"></stop></linearGradient></defs><path d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z" fill="url(#fillStyle-rating3-star-3)"></path></svg>
+                </div>
+                <p class="review_text">
+                  "(Translated by Google)
+God (Original) Gud"
+                </p>
+                <div class="date">
+                  <div class="d-flex justify-content-center align-items-center">
+                    <svg viewBox="0 0 24 24" fill="currentColor" width="28" height="28" data-ux="Icon" class="x-el x-el-svg c2-1 c2-2 c2-1d c2-r c2-s c2-3 c2-4 c2-5 c2-6 c2-7 c2-8"><g fill="none" fill-rule="evenodd" transform="translate(2 2)"><circle cx="10" cy="10" r="9.5" fill="#F3F3F3" stroke="#CCC"></circle><g transform="translate(5.556 5.5)"><path fill="#4285F4" d="M8.494 4.375c0-.303-.028-.595-.079-.875H4.334v1.655h2.332c-.1.535-.406.988-.865 1.291V7.52h1.4c.82-.744 1.293-1.841 1.293-3.144z"></path><path fill="#34A853" d="M4.334 8.556c1.17 0 2.15-.383 2.868-1.037l-1.4-1.073c-.389.257-.885.408-1.468.408-1.129 0-2.084-.752-2.425-1.763H.46v1.108a4.338 4.338 0 0 0 3.873 2.357z"></path><path fill="#FBBC05" d="M1.909 5.09a2.542 2.542 0 0 1-.136-.812c0-.282.05-.556.136-.813V2.357H.46a4.231 4.231 0 0 0 0 3.842l1.448-1.108z"></path><path fill="#EA4335" d="M4.334 1.701c.636 0 1.207.216 1.656.64l1.243-1.227C6.483.424 5.502 0 4.333 0 2.64 0 1.175.959.462 2.357l1.448 1.108c.34-1.011 1.296-1.764 2.425-1.764z"></path></g></g></svg>
+                    <p class="ms-3 mb-0"><strong>rav inder -</strong> <span>20/1/2018</span></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer class="footer_section py-4">
+      <div class="container">
+        <div class="copyright text-center">
+          <p>Copyright © 2022 Travel Freeby - All Rights Reserved.</p>
+          <ul>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Cookie Policy</a></li>
+            <li><a href="#">Terms of use</a></li>
+            <li><a href="#">User Agreement</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+     <!-- external section -->
+     <div id="chat"  class="fixed_message">
+      <div class="chat_container">
+        <div class="chat_header">
+          <h2>Travel Freeby</h2>
+        </div>
+        <div id="chat_input">
+          <form action="" method="get">
+            <input type="text" name="" id="" placeholder="Name">
+            <input type="email" name="" id="" placeholder="Email">
+            <textarea name="" id="" placeholder="How can we help?*"></textarea>
+            <div class="text-center">
+              <button type="submit">SEND</button>
+            </div>
+          </form>
+          <p class="text-center">This site is protected by reCAPTCHA and the Google <a href="#">Privacy Policy</a> and <a href="#">Terms of Service</a> apply.</p>
+        </div>
+      </div>
+      <div class="chat_icon">
+        <i id="chatIcon" onclick="chatToggle('chat','chatIcon')" class="fa fa-comment"></i>
+      </div>
+    </div>
+
+    <script src="assetsh/js/welcome.js"></script>
+  </body>
+</html>
